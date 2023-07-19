5c03710cdcfe28585bcc457c704e86014f8a3517
felixonrocks <lif.rochel@gmail.com>
2023-07-19 12:17:42 +0200

    design half startpage

---
+++ resources/views/inner.blade.php
@@ -0,0 +1,24 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+ </head>
+ <>
+
+<div class="bg-gray-200 w-20 h-16 shadow-lg border text-white rounded text-sm pl-2"  >
+    i am an internal document
+</div>
+
+<img src="/img/">
+
+
+</body>
+</html>
+
+<script type="text/javascript">
+
+var s = 'prize won!!';
+
+</script>
